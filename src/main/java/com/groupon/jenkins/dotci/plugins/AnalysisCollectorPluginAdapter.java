7286ac530272cba32431ca28ba490c3c048c0538
Surya Gaddipati <surya.gaddipati@gmail.com>
2016-09-21 19:47:40 -0500

    reformat and cleanup imports

--- src/main/java/com/groupon/jenkins/dotci/plugins/AnalysisCollectorPluginAdapter.java
+++ src/main/java/com/groupon/jenkins/dotci/plugins/AnalysisCollectorPluginAdapter.java
@@ -1,22 +1,36 @@
 package com.groupon.jenkins.dotci.plugins;
 
-import com.google.common.base.*;
-import com.google.common.collect.*;
-import com.groupon.jenkins.buildtype.plugins.*;
-import com.groupon.jenkins.dotci.patch.*;
-import com.groupon.jenkins.dynamic.build.*;
-import com.groupon.jenkins.github.services.*;
-import hudson.*;
-import hudson.model.*;
-import hudson.plugins.analysis.collector.*;
-import hudson.plugins.analysis.core.*;
-import hudson.plugins.analysis.util.model.*;
-import hudson.plugins.cobertura.*;
-import hudson.plugins.cobertura.targets.*;
-import org.kohsuke.github.*;
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+import com.groupon.jenkins.buildtype.plugins.DotCiPluginAdapter;
+import com.groupon.jenkins.dotci.patch.PatchFile;
+import com.groupon.jenkins.dotci.patch.PatchHunk;
+import com.groupon.jenkins.dotci.patch.PatchLine;
+import com.groupon.jenkins.dotci.patch.PatchParser;
+import com.groupon.jenkins.dynamic.build.DynamicBuild;
+import com.groupon.jenkins.github.services.GithubRepositoryService;
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.plugins.analysis.collector.AnalysisPublisher;
+import hudson.plugins.analysis.core.AbstractResultAction;
+import hudson.plugins.analysis.core.BuildResult;
+import hudson.plugins.analysis.util.model.FileAnnotation;
+import hudson.plugins.cobertura.CoberturaBuildAction;
+import hudson.plugins.cobertura.targets.CoveragePaint;
+import hudson.plugins.cobertura.targets.CoverageResult;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHPullRequestReviewComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.PagedIterable;
 
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Extension
 public class AnalysisCollectorPluginAdapter extends DotCiPluginAdapter {
@@ -25,104 +39,102 @@ public class AnalysisCollectorPluginAdapter extends DotCiPluginAdapter {
     }
 
     @Override
-    public boolean perform(DynamicBuild dynamicBuild, Launcher launcher, BuildListener listener) {
+    public boolean perform(final DynamicBuild dynamicBuild, final Launcher launcher, final BuildListener listener) {
         try {
-            new AnalysisPublisher().perform(((AbstractBuild)dynamicBuild), launcher, listener);
-        } catch (InterruptedException e) {
+            new AnalysisPublisher().perform(((AbstractBuild) dynamicBuild), launcher, listener);
+        } catch (final InterruptedException e) {
             throw new RuntimeException(e);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw new RuntimeException(e);
         }
-        if(  dynamicBuild.isPullRequest()){
+        if (dynamicBuild.isPullRequest()) {
             try {
-                int prNumber = Integer.parseInt(dynamicBuild.getCause().getPullRequestNumber());
-                List<PatchFile> patchFiles = new PatchParser(listener).getLines(dynamicBuild.getGithubRepoUrl(), prNumber);
-                List<LineComment> lineComments = new ArrayList<LineComment>();
-                AbstractResultAction action = dynamicBuild.getAction(AbstractResultAction.class);
-                if(action == null)return true;
-                BuildResult anaylsisResult = action.getResult();
-                CoberturaBuildAction cobeturaAction = dynamicBuild.getAction(CoberturaBuildAction.class);
-                CoverageResult coberturaResult = cobeturaAction == null ? null : cobeturaAction.getResult();
-                PrintStream logger = listener.getLogger();
-                for (PatchFile file : patchFiles) {
-                    String fileName = file.getFilename();
-                    for (PatchHunk hunk : file.getHunks()) {
-                        if(coberturaResult!=null){
-                            LineComment coverageComment = coverageComment(hunk,fileName,coberturaResult);
-                            if(coverageComment !=null){
+                final int prNumber = Integer.parseInt(dynamicBuild.getCause().getPullRequestNumber());
+                final List<PatchFile> patchFiles = new PatchParser(listener).getLines(dynamicBuild.getGithubRepoUrl(), prNumber);
+                final List<LineComment> lineComments = new ArrayList<>();
+                final AbstractResultAction action = dynamicBuild.getAction(AbstractResultAction.class);
+                if (action == null) return true;
+                final BuildResult anaylsisResult = action.getResult();
+                final CoberturaBuildAction cobeturaAction = dynamicBuild.getAction(CoberturaBuildAction.class);
+                final CoverageResult coberturaResult = cobeturaAction == null ? null : cobeturaAction.getResult();
+                final PrintStream logger = listener.getLogger();
+                for (final PatchFile file : patchFiles) {
+                    final String fileName = file.getFilename();
+                    for (final PatchHunk hunk : file.getHunks()) {
+                        if (coberturaResult != null) {
+                            final LineComment coverageComment = coverageComment(hunk, fileName, coberturaResult);
+                            if (coverageComment != null) {
                                 lineComments.add(coverageComment);
                             }
                         }
-                        List<LineComment> analysisComments = getAnalysisComments(anaylsisResult, fileName, hunk);
+                        final List<LineComment> analysisComments = getAnalysisComments(anaylsisResult, fileName, hunk);
                         lineComments.addAll(analysisComments);
                     }
 
                 }
-                GHRepository repo = new GithubRepositoryService(dynamicBuild.getGithubRepoUrl()).getGithubRepository();
-                GHPullRequest pullRequest = repo.getPullRequest(prNumber);
-                PagedIterable<GHPullRequestReviewComment> allReviewComments = pullRequest.listReviewComments();
-                for(LineComment comment : lineComments){
-                    makeComment(allReviewComments,logger, pullRequest, comment);
+                final GHRepository repo = new GithubRepositoryService(dynamicBuild.getGithubRepoUrl()).getGithubRepository();
+                final GHPullRequest pullRequest = repo.getPullRequest(prNumber);
+                final PagedIterable<GHPullRequestReviewComment> allReviewComments = pullRequest.listReviewComments();
+                for (final LineComment comment : lineComments) {
+                    makeComment(allReviewComments, logger, pullRequest, comment);
                 }
 
-//                if(anaylsisResult.getNewWarnings().size() > 0){
-//                    String warningMessage = String.format("Analysis Summary: \n\n * Added:  __%s__\n * Fixed: __%s__ \n * Total: __%s__", anaylsisResult.getNumberOfNewWarnings(), anaylsisResult.getNumberOfFixedWarnings(), anaylsisResult.getNumberOfWarnings());
-//                    pullRequest.comment(warningMessage);
-//                }
-            } catch (IOException e) {
+            } catch (final IOException e) {
                 throw new RuntimeException(e);
             }
         }
         return true;
     }
 
-    private void makeComment(PagedIterable<GHPullRequestReviewComment> allReviewComments, PrintStream logger, GHPullRequest pullRequest, final LineComment comment) throws IOException {
-        GHPullRequestReviewComment existingComment = Iterables.find(allReviewComments, new Predicate<GHPullRequestReviewComment>() {
+    private void makeComment(final PagedIterable<GHPullRequestReviewComment> allReviewComments, final PrintStream logger, final GHPullRequest pullRequest, final LineComment comment) throws IOException {
+        final GHPullRequestReviewComment existingComment = Iterables.find(allReviewComments, new Predicate<GHPullRequestReviewComment>() {
             @Override
-            public boolean apply(GHPullRequestReviewComment reviewComment) {
+            public boolean apply(final GHPullRequestReviewComment reviewComment) {
                 return comment.isSameAs(reviewComment);
             }
         }, null);
-        if(existingComment == null){
+        if (existingComment == null) {
             logger.println("Commenting on " + comment.line.getLineNo() + " at Pos: " + comment.line.getPos());
             pullRequest.createReviewComment(comment.comment, pullRequest.getHead().getSha(), comment.fileName, comment.line.getPos());
         }
     }
 
-    private List<LineComment> getAnalysisComments(BuildResult anaylsisResult, String fileName, PatchHunk hunk) {
-        List<LineComment> lineComments = new ArrayList<LineComment>();
-        for (PatchLine line : hunk.getLines()) {
-            FileAnnotation annotation = findAnnotation(anaylsisResult, fileName, line.getLineNo());
+    private List<LineComment> getAnalysisComments(final BuildResult anaylsisResult, final String fileName, final PatchHunk hunk) {
+        final List<LineComment> lineComments = new ArrayList<>();
+        for (final PatchLine line : hunk.getLines()) {
+            final FileAnnotation annotation = findAnnotation(anaylsisResult, fileName, line.getLineNo());
             if (annotation != null) {
-                String message = annotation.getMessage();
-                lineComments.add(new LineComment(line,message,fileName));
+                final String message = annotation.getMessage();
+                lineComments.add(new LineComment(line, message, fileName));
             }
         }
         return lineComments;
     }
 
-    private LineComment coverageComment(PatchHunk hunk, String fileName, CoverageResult coverageResult) {
-        Map<String, CoveragePaint> paintedSources = coverageResult.getPaintedSources();
-        for(String coverageFileName: paintedSources.keySet()){
-            if(fileName.contains(coverageFileName)){
-                CoveragePaint coverage = paintedSources.get(coverageFileName);
-                List<String> unCoveredLines = new ArrayList<String>();
-                for(PatchLine line:hunk.getLines()){
-                   if(coverage.getHits(line.getLineNo()) == 0) unCoveredLines.add(line.getLineNo()+"");
+    private LineComment coverageComment(final PatchHunk hunk, final String fileName, final CoverageResult coverageResult) {
+        final Map<String, CoveragePaint> paintedSources = coverageResult.getPaintedSources();
+        for (final String coverageFileName : paintedSources.keySet()) {
+            if (fileName.contains(coverageFileName)) {
+                final CoveragePaint coverage = paintedSources.get(coverageFileName);
+                final List<String> unCoveredLines = new ArrayList<>();
+                for (final PatchLine line : hunk.getLines()) {
+                    if (coverage.getHits(line.getLineNo()) == 0)
+                        unCoveredLines.add(line.getLineNo() + "");
                 }
-                if(unCoveredLines.size() > 0){
-                   String message = "Missing coverage for line(s) :  ```"+ Joiner.on(" ").join(unCoveredLines)+"```";
-                   return new LineComment(Iterables.getLast(hunk.getLines()),message,fileName) ;
+                if (unCoveredLines.size() > 0) {
+                    final String message = "Missing coverage for line(s) :  ```" + Joiner.on(" ").join(unCoveredLines) + "```";
+                    return new LineComment(Iterables.getLast(hunk.getLines()), message, fileName);
                 }
             }
 
         }
         return null;
     }
-    private FileAnnotation findAnnotation(BuildResult buildResult, String fileName, int lineNo) {
-        for(FileAnnotation annotation: buildResult.getAnnotations()){
-            if(annotation.getFileName().endsWith(fileName) ){
-                if(annotation.getPrimaryLineNumber() == lineNo){
+
+    private FileAnnotation findAnnotation(final BuildResult buildResult, final String fileName, final int lineNo) {
+        for (final FileAnnotation annotation : buildResult.getAnnotations()) {
+            if (annotation.getFileName().endsWith(fileName)) {
+                if (annotation.getPrimaryLineNumber() == lineNo) {
                     return annotation;
                 }
 //                for(LineRange range : annotation.getLineRanges()){
@@ -134,21 +146,22 @@ public class AnalysisCollectorPluginAdapter extends DotCiPluginAdapter {
         }
         return null;
     }
-    private static class LineComment{
+
+    private static class LineComment {
         public PatchLine line;
         public String comment;
         public String fileName;
 
-        public LineComment(PatchLine line, String comment, String fileName) {
+        public LineComment(final PatchLine line, final String comment, final String fileName) {
             this.line = line;
             this.comment = comment;
             this.fileName = fileName;
         }
 
-        public boolean isSameAs(GHPullRequestReviewComment reviewComment) {
-            return reviewComment.getPosition() == line.getPos()
-                    && comment.equals(reviewComment.getBody())
-                    && fileName.equals(reviewComment.getPath());
+        public boolean isSameAs(final GHPullRequestReviewComment reviewComment) {
+            return reviewComment.getPosition() == this.line.getPos()
+                    && this.comment.equals(reviewComment.getBody())
+                    && this.fileName.equals(reviewComment.getPath());
         }
     }
 }
